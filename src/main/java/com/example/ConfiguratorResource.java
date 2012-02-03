8f9500eae085e70b33a9753519a2416e47010cd1
Shan Lyons <shan.lyons@akqa.com>
2012-02-03 09:34:52 -0800

    Processor removed and clarity in route flow to returned HTML established.

--- src/main/java/com/example/ConfiguratorResource.java
+++ src/main/java/com/example/ConfiguratorResource.java
@@ -1,19 +1,16 @@
 package com.example;
 
+import java.util.Date;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-
-import org.apache.log4j.Logger;
 
 @Path("/example")
 public class ConfiguratorResource {
 
-	private static final Logger LOGGER = Logger.getLogger(ConfiguratorResource.class);
-
 	@GET
 	@Path("/")
 	public String ping() {
-		LOGGER.debug("Inside ConfiguratorResource.ping()");
-		return "SUCCESS";
+		return "This string is set in Configurator Resource at " + new Date().toString();
 	}
 }
--- src/main/java/com/example/ConfiguratorRouter.java
+++ src/main/java/com/example/ConfiguratorRouter.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 public class ConfiguratorRouter extends RouteBuilder {
@@ -7,6 +9,11 @@ public class ConfiguratorRouter extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		from("cxfrs://http://localhost:9000?resourceClasses=" + ConfiguratorResource.class.getName())
-			.setBody(constant("SUCCESS"));
+			.process(new Processor() {
+			    @Override
+                public void process(Exchange exchange) {
+			        exchange.getIn();
+			    }
+			});
 	}
 }
