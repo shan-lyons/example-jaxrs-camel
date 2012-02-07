b9bdeb2ce44f8d06dde4c4a5d4fde5c8dfe7c049
Shan Lyons <shan.lyons@akqa.com>
2012-02-07 15:33:27 -0800

    Addition of CXFRS infrastructure and Product Data Routing.

--- src/main/java/com/example/ConfiguratorRouter.java
+++ src/main/java/com/example/ConfiguratorRouter.java
@@ -7,23 +7,29 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.example.cxf.model.Trimline;
 import org.apache.camel.model.dataformat.JsonLibrary;
 
+/**
+ * Defines routes for Configurator Product Data.
+ *
+ * @author shan.lyons
+ *
+ */
 public class ConfiguratorRouter extends RouteBuilder {
     private static final String AUTH_PARAMS = "authMethod=Basic&authUsername=sfaudidev&authPassword=qwerm,./!";
     private static final String DATA_URI = "http://staging.models.audicatalyst.com"
             + "/services/carlines/a7/configurator.json"
             + "?bridgeEndpoint=true&throwExceptionOnFailure=false&" + AUTH_PARAMS;
 
-	@Override
-	public void configure() throws Exception {
-		from("cxfrs:bean:rsServer")
-		    .setHeader(Exchange.HTTP_PATH, constant(""))
-		    // There must be a better way to do prevent the conversion error than
-		    // setting the body to empty before the data request...
-		    .setBody(constant(""))
+    @Override
+    public void configure() throws Exception {
+        from("cxfrs:bean:rsServer")
+            .setHeader(Exchange.HTTP_PATH, constant(""))
+            // There must be a better way to do prevent the conversion error than
+            // setting the body to empty before the data request...
+            .setBody(constant(""))
             .to(DATA_URI)
             .unmarshal().gzip()
             .unmarshal().json(JsonLibrary.Jackson, Trimline[].class)
             .process(new ProductDataProcessor())
             .marshal().json(JsonLibrary.Jackson, InputStream.class);
-	}
+    }
 }
